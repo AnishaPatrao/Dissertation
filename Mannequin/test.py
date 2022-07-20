1daa10c33194af8235c7999b0753194a861a8826
Anisha <a.m.patrao2@newcastle.ac.uk>
2022-07-20 18:34:56 +0100

    added second servo

--- Mannequin/test.py
+++ Mannequin/test.py
@@ -1,17 +1,13 @@
 import RPi.GPIO as GPIO
 import time
 
-servoPIN1 = 10
-servoPIN2 = 19
+servoPIN2 = 18
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN1, GPIO.OUT)
 GPIO.setup(servoPIN2, GPIO.OUT)
 
-p = GPIO.PWM(servoPIN1, 50) # GPIO 10 for PWM with 50Hz
 p2 = GPIO.PWM(servoPIN2, 50) # GPIO 14 for PWM with 50Hz
-p.start(7.5) # Initialization
-p2.start(12.5) # Initialization
-time.sleep(2.5)
+p2.start(7.5) # Initialization
+time.sleep(0.5)
 """ p.ChangeDutyCycle(7.5)
 time.sleep(0.5) """
 
@@ -20,11 +16,9 @@ def Move(position):
   try:
     print(position)
     if position == 'bent':
-      p.ChangeDutyCycle(2.5)
       p2.ChangeDutyCycle(7.5)
       time.sleep(0.5)
     else:
-      p.ChangeDutyCycle(7.5)
       p2.ChangeDutyCycle(12.5)
       time.sleep(0.5)
   except KeyboardInterrupt:
@@ -33,7 +27,11 @@ def Move(position):
     GPIO.cleanup()
 
 
-""" 
+
+Move('str')
+time.sleep(1)
+""" Move('str')
+time.sleep(1)
 Move('bent')
 time.sleep(1)
 Move('str')
@@ -52,8 +50,4 @@ Move('str')
 time.sleep(1)
 Move('bent')
 time.sleep(1)
-Move('str')
-time.sleep(1)
-Move('bent')
-time.sleep(1)
-Move('str')   """
+Move('str') """
